7eb63f75b3cd9cec4ce4ecd7209d1fd6099ccc0c
Franco Piccolo <francopiccolo@gmail.com>
2024-05-28 20:15:08 +0300

    Agregue etiqueta other para exportadores con bajo volumen

--- view/bilateral_trade_flows.sql
+++ view/bilateral_trade_flows.sql
@@ -1,9 +1,15 @@
 DROP VIEW IF EXISTS views.bilateral_trade_flows;
 
 CREATE VIEW views.bilateral_trade_flows AS (
+WITH data AS (
+SELECT  exporter_country_code, SUM(trade_value_current_usd) AS total_usd_per_country
+FROM    fact.bilateral_trade_flows
+GROUP   BY 1
+)
+
 
 SELECT  tf.year, 
-        ce.country          AS exporter_country,
+        CASE WHEN  total_usd_per_country < 333953450236 THEN 'Other' ELSE ce.country END AS exporter_country,
         ci.country          AS importer_country,
         pc.*,
         tf.trade_value_current_usd,
@@ -12,4 +18,5 @@ FROM    fact.bilateral_trade_flows tf
 JOIN    dim.country ce ON ce.country_code = tf.exporter_country_code
 JOIN    dim.country ci ON ci.country_code = tf.importer_country_code
 JOIN    dim.product_category pc ON pc.product_category_l4_code = tf.product_category
+JOIN    data d ON d.exporter_country_code = tf.exporter_country_code
 );
